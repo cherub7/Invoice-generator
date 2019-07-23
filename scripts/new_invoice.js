ecbcf9b43ba6b71df8df1bc3d85956a15284c299
Cherubim <ch3rubim7@gmail.com>
2019-07-23 23:57:35 +0530

    dev info
    added dev info card to index page

--- scripts/new_invoice.js
+++ scripts/new_invoice.js
@@ -224,7 +224,7 @@ function sendToFirestore(data) {
         time: firebase.firestore.Timestamp.fromDate(new Date())
     })
     .then(function(docRef) {
-        alert("Successfully added document to cloud.");
+        aleralert("Successfully added document to cloud.");
     })
     .catch(function(error) {
         alert("Error adding document to cloud.");
